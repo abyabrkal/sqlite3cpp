d5525b87bcd153abe89776d085fc10cf47f640ec
ABY ABRAHAM KALATHIVILA <abyabrkal@gmail.com>
2018-11-29 10:11:04 +0400

    cmake command created some VStudio sln extensions. need work

---
+++ cmakeintro/cmintroconfig.h
@@ -0,0 +1,7 @@
+/* the configured options and settings for cmakeintro
+
+   Since the configured file will be written into the binary tree we must add that directory to the list of paths to search for include files.
+*/
+#define cmintro_VERSION_MAJOR 
+#define cmintro_VERSION_MINOR 
+#define USE_MYMATH
